e0c96ccd9ea9ef1e51bf79358930be2190aeff24
gunhong951 <gunhong951@gmail.com>
2023-06-10 13:54:30 +0900

    [fix] 불필요한 packing unpacking 수정

--- 컴퓨터가 되어버린 운영진을 이겨라.py
+++ 컴퓨터가 되어버린 운영진을 이겨라.py
@@ -28,22 +28,22 @@ class Game:
         self.you = Player(self.players[your_index])
         self.computers = [Player(player) for player in self.players if player != self.players[your_index]]
         for round in range(1,5):
-            self.play_round(round, self.you, *self.computers)
+            self.play_round(round, self.you, self.computers)
         # self.game_result()
             
-    def play_round(self, round, you, *computers): # 라운드 시작할때마다 덱을 생성해야겠지? 그리고 카드를 뽑아야할듯
+    def play_round(self, round, you, computers:list[Player]): # 라운드 시작할때마다 덱을 생성해야겠지? 그리고 카드를 뽑아야할듯
         print("===========================")
         print(f"     ROUND {round} - START")
         print("===========================")
-        play_list: list[Player] = self.set_play_order(round, you, *computers)
+        play_list: list[Player] = self.set_play_order(round, you, computers)
         # deck을 새롭게 초기화
         card_deck = [random.randint(1, 13) for _ in range(30)]
         print(f"게임은 {', '.join(str(player) for player in play_list)} 순으로 진행됩니다.\n")
         self.play_game(play_list, card_deck)
         return 0
     
-    def set_play_order(self, round, you, *computers): # 첫라운드는 이름순, 그 후는 점수가 낮은 순서대로 카드를 뽑는다.
-        players: list[Player] = [*computers]
+    def set_play_order(self, round, you:Player, computers: list[Player]): # 첫라운드는 이름순, 그 후는 점수가 낮은 순서대로 카드를 뽑는다.
+        players = [*computers] # 언패킹후 다시 리스트로 감싸준다.
         players.append(you)
         if round == 1:
             ascending_order = sorted(players, key=lambda x : x.name) # 객체인데... 객체안의 name순서로 정렬을 할수는 없을까... lambda을 이용하면 되는구나
