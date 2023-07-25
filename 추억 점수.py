2cb14c2e5dc4502f2b79bab9f6156600f603180a
gunhong <gunhong951@gmail.com>
2023-07-25 15:12:14 +0900

    추억 점수 (zip을 활용)

--- 추억 점수.py
+++ 추억 점수.py
@@ -1,6 +1,7 @@
 def solution(name, yearning, photos):
     result = []
     dic= {n:yearning[idx] for idx, n in enumerate(name) } # 이렇게 해봤자 시간 복잡도 100이 최대?
+    # yearning_table = dict(zip(name, yearning)) # zip을 이용하는 방법도 좋았겠어...
     print(dic)
     for photo in photos:
         sub_result = 0
