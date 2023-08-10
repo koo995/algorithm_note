83dc49b7afc5c26f5a8663f0ef05d6fe4d69ac78
gunhong951 <gunhong951@gmail.com>
2023-08-10 13:36:36 +0900

    2번째 풀이

--- 바탕화면정리.py
+++ 바탕화면정리.py
@@ -1,26 +1,20 @@
 def solution(wallpaper):
-    min_x = int(1e9)
-    min_y = int(1e9)
-    max_x = -int(1e9)
-    max_y = -int(1e9)
-    
-    for w_y, row in enumerate(wallpaper):
-        # 전체 모양 출력
-        print(w_y, row)
-        
-        # row에서 #을 찾는다. 그리고 최대 최소 갱신
-        index = row.find("#")
-        if index != -1: # 샵을 찾는다 없으면 -1, 찾으면 index 반환
-            laxt_index = row.rfind("#")
-            w_x = index
-            w_x_r = laxt_index
-            min_x = min(min_x, w_x)
-            min_y = min(min_y, w_y)
-            max_x = max(max_x, w_x_r + 1)
-            max_y = max(max_y, w_y + 1)
-            
-    answer = [min_y, min_x, max_y, max_x]
+    INF = int(1e9)
+    minY = INF
+    minX = INF
+    maxY = -INF
+    maxX = -INF
+    for y, row in enumerate(wallpaper):
+        if "#" in row:
+            min_x = row.find("#")
+            max_x = row.rfind("#")
+            minY = min(y, minY)
+            minX = min(min_x, minX)
+            maxY = max(y, maxY)
+            maxX = max(max_x, maxX)
+    answer = [minY, minX, maxY+1, maxX+1]
     return answer
+ # 아하 하나의 row에서 제일 끝에 있는 경우도 있을 수 있군...
 
 
 # max에서는 x, y 에 1을 추가해줘야 포함될듯
