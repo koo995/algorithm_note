e74b071fd3bc4d6781f5e5c83b83953a6e2f08cf
gunhong951 <gunhong951@gmail.com>
2023-08-12 17:15:47 +0900

    1로 만들기 2번째

--- 1로 만들기_bj.py
+++ 1로 만들기_bj.py
@@ -1,18 +1,20 @@
 n = int(input())
-dp = [0] * int(1e6 +1) # 메모리제한이 어짜피 128이니까 10^6까지 그냥 만들어 놔도 될듯
-dp[0] = 0
+INF = int(1e6)
+dp = [INF] * (1000001)
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
-for i in range(4, n+4):
-    min_prev = dp[i-1]
-    if i % 3 == 0:
-        min_prev = min(min_prev, dp[int(i//3)])
+
+for i in range(4, n+1):
+    if dp[i-1] != INF:
+        dp[i] = dp[i-1] + 1 # 먼저 1을 뺀 녀석을 시작한다.
     if i % 2 == 0:
-        min_prev = min(min_prev, dp[int(i//2)])
-    dp[i] = min_prev + 1
+        dp[i] = min(dp[i], dp[i//2] + 1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
 
 print(dp[n])
+
 
 # n이 1 이나 2 3인경우가 문제구나
 # 그냥 3칸만 더 구하면 되지않나?
