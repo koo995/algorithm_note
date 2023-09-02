3f7082402d9a5ce1c712d9d80ba8be8b6e192e03
gunhong951 <gunhong951@gmail.com>
2023-09-02 16:59:44 +0900

    어떤 반례 때문일까

--- AC.py
+++ AC.py
@@ -2,7 +2,6 @@ from collections import deque
 
 T = int(input())
 cases = [[input() for _ in range(3)]  for _ in range(T)]
-# [['RDD', '4', '[1,2,3,4]'], ['DD', '1', '[42]'], ['RRD', '6', '[1,1,2,3,5,8]'], ['D', '0', '[]']]
 
 for case in cases:
     case = [case[0], int(case[1]), list(map(int,case[2][1:-1].split(","))) if len(case[2]) > 2 else []]
@@ -10,25 +9,34 @@ for case in cases:
     n = case[1]
     arr = deque(case[2])
     flag = True
+    reverse = False
+    R_count = 0
     
     for func in functions:
         if func == "D": # 버리기
             if len(arr) != 0 :
-                arr.popleft() # 인덱스를 지정해서 그렇구나...
+                if reverse == False:
+                    arr.popleft() # 인덱스를 지정해서 그렇구나...
+                else:
+                    arr.pop()
             else:
                 print("error")
                 flag = False
                 break
-            # print("버린 arr: ", arr)
             
         else: # func == "R" 뒤집기
-            arr = deque([arr.pop() for _ in range(len(arr))])
-            # print("뒤집은 arr: ", arr)
+            R_count += 1
+            reverse = True if R_count % 2 != 0 else False
     if flag == False:
         continue
     
-    print(list(arr))
+    result = list(arr)
+    if R_count % 2 != 0:
+        result.reverse()
+    print(result)
 
 
 # 시간 초과가 발생하네... 뒤집기에서 문제가 생기는 것 같아.
 # 어디서 뭘 어떻게 줄여야 하지? 뒤집기에서 로그연산을 하면 시간복잡도가 딱 알맞는듯
+# boolean 변수로 R의 갯수에 따라 앞에서 뒤에서 pop() 하는 방식을 썼는데...
+# 어떤 케이스에서 틀린걸까?
