d1d95d4a8f920991e6814e3306130dcf0ab26c2a
gunhong951 <gunhong951@gmail.com>
2023-09-04 19:15:05 +0900

    (add) 최소힙 (python3로 제출하니 시간초과 발생)

---
+++ 최소힙.py
@@ -0,0 +1,19 @@
+import heapq
+# 파이썬에서 시간초과 뜨는 경우 
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # (1~100000)
+arr = [int(input()) for _ in range(N)]
+
+heap = []
+for e in arr:
+    if e != 0:
+        heapq.heappush(heap, e)
+    else: # input이 0인 경우? 가장 작은 값을 출력해야지
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    
+# 시간초과가 발생하네...
