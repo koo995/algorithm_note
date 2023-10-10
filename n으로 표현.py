2424e70fcdc7b7efbf6c583a4fa3aca6edf71e4f
gunhong951 <gunhong951@gmail.com>
2023-10-10 19:00:42 +0900

    [FEAT] n으로 표현 (절반의 정확도)

--- n으로 표현.py
+++ n으로 표현.py
@@ -2,39 +2,42 @@ from collections import deque
 
 def solution(N, number):
     INF = 1e9
-    dp = [INF] * (32000)
+    dp = [INF] * (32001)
     dp[N] = 1 # 동전을 한개만 쓴 경우
     stack = deque()
-    stack.append(N)
-    for i in range(2,9): # 동전을 2개만 썼을때 에서 8개 썼을 때까지
-        while stack:
-            e = stack.popleft()
-            # concat
-            e_cat_N = int(str(e) + str(N))
-            if e_cat_N < 32000 and dp[e_cat_N] > i:
-                dp[e_cat_N] = i
-                stack.append(e_cat_N)
-            # 곱하기
-            e_pow_N = e*N
-            if e_pow_N < 32000 and dp[e_pow_N] > i:
-                dp[e_pow_N] = i
-                stack.append(e_pow_N)
-            # 나누기
-            if e % N == 0 and dp[int(e/N)] > i:
-                dp[int(e/N)] = i
-                stack.append(int(e/N))
-            # 더하기
-            e_plus_N = e + N
-            if e+N < 32000 and dp[e_plus_N] > i:
-                dp[e_plus_N] = i
-                stack.append(e_plus_N)
-            # 빼기
-            if e - N > 0 and dp[e-N] > i:
-                dp[e - N] = i
-                stack.append(e-N)            
+    stack.append((1,N))
+    while stack:
+        i, e = stack.popleft()
+        if i >= 8:
+            break
+        # concat
+        e_cat_N = int(str(e) + str(N))
+        if e_cat_N < 32001:
+            dp[e_cat_N] = min(dp[e_cat_N], i+1)
+            stack.append((i+1,e_cat_N))
+        # 곱하기
+        e_pow_N = e*N
+        if e_pow_N < 32001:
+            dp[e_pow_N] = min(dp[e_pow_N], i+1)
+            stack.append((i+1, e_pow_N))
+        # 나누기
+        if 0 <= int(e/N) < 32001:
+            dp[int(e/N)] = min(dp[int(e/N)], i+1)
+            stack.append((i+1,int(e/N)))
+        # 더하기
+        e_plus_N = e + N
+        if e+N < 32001:
+            dp[e_plus_N] = min(dp[e_plus_N], i+1)
+            stack.append((i+1,e_plus_N))
+        # 빼기
+        if e - N >= 0:
+            dp[e - N] = min(dp[e - N], i+1)
+            stack.append((i+1, e-N))
     return dp[number] if dp[number] <= 8 else -1
-print(dp[:20])
+    print(dp[:20])
 print(solution(5, 12))
+print(solution(5, 3025))
 # print(solution(2, 11))
-
 # "/" 연산은 float을 반환하나 보네
+# 무한 루프에 빠지는 이유는 뭐지? 동전의 갯수가 업데이트가 안되는 구나
+# 나머지는 무시한다는데...
