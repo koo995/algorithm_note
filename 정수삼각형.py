286d584a6188bdb489d580dc7b4e58aaecd65c2b
gunhong951 <gunhong951@gmail.com>
2023-11-21 12:19:25 +0900

    fix: 정수삼각형

--- 정수삼각형.py
+++ 정수삼각형.py
@@ -1,13 +1,34 @@
 n = int(input())
-costs = [ list(map(int,input().split())) for _ in range(n)]
-dp = [[0] * (i+1) for i in range(n)]
+costs = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (i + 1) for i in range(n)]
 dp[0] = costs[0]
 for i in range(1, n):
-    dp[i][0] = dp[i-1][0] + costs[i][0]
+    dp[i][0] = dp[i - 1][0] + costs[i][0]
 for i in range(1, n):
     l = len(costs[i])
-    for j in range(1,l):
-        dp[i][j] = (max(dp[i-1][j-1], dp[i-1][j]) if j!=(l-1) else dp[i-1][j-1])  + costs[i][j]
+    for j in range(1, l):
+        dp[i][j] = (
+            max(dp[i - 1][j - 1], dp[i - 1][j]) if j != (l - 1) else dp[i - 1][j - 1]
+        ) + costs[i][j]
 print(max(dp[-1]))
 
 # 제일 끝 부분에 있는 녀석은 max비교로 넣을 것이 아니군
+
+
+def solution(triangle):
+    dp_triangle = [[0] + i + [0] for i in triangle]
+    print("dp+triangle", dp_triangle)
+    for idx, triangle_line in enumerate(
+        dp_triangle
+    ):  # 아하... 이렇게 되었을 때도 idx가 0부터 나오는 구나...
+        if idx == 0:
+            continue
+        for j in range(1, len(triangle_line) - 1):
+            print("idx, j: ", idx, j)
+            dp_triangle[idx][j] += max(
+                dp_triangle[idx - 1][j - 1], dp_triangle[idx - 1][j]
+            )
+    return max(dp_triangle[-1])
+
+
+print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
