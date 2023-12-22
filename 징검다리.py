49df2c1b8d98c592350e77a932ef12fadb03387f
gunhong951 <gunhong951@gmail.com>
2023-12-22 17:27:54 +0900

    Fix 징검다리

--- 징검다리.py
+++ 징검다리.py
@@ -1,5 +1,4 @@
 import math
-
 import time
 
 
@@ -41,4 +40,35 @@ def solution(distance, rocks, n):
     return bigger_than_answer[0]
 
 
-print(solution(25, [2, 14, 11, 21, 17], 2))
+def solution1(distance, rocks, n):
+    answer = 0
+    sorted_rocks = sorted(rocks)
+    sorted_rocks.append(distance)  # 마지막 도착지점을 위해서 넣었나 보군.
+    # 역시나 길이로 이분탐색을 적용해 나간다.
+    left = 0
+    right = distance
+    while left <= right:
+        mid = int((left + right) / 2)  # mid 이 녀석을 어쨋든 최소로 잡고싶어 하는 것이지?
+        cnt = 0
+        rock_point = 0
+        # 어쨋든 간에 모든 거리에 대해서 하나하나 확인해 나간다?
+        for i in range(len(sorted_rocks)):
+            if sorted_rocks[i] - rock_point < mid:
+                cnt += 1
+            else:  # 여기 이 부분이 내가 생각하지 못한 부분이구나?
+                rock_point = sorted_rocks[i]
+        if cnt > n:  # 여기서 1로 두는 것도 음... 등호를 안쓰고 종료조건을 저렇게 만들어나가나?
+            right = mid - 1
+        else:
+            left = mid + 1
+            answer = mid
+    return answer
+
+
+print(solution1(25, [2, 14, 11, 21, 17], 2))
+print(solution1(28, [2, 9, 14, 11, 21, 17, 20], 4))
+
+"""
+만약에 돌멩이 사이의 거리를 최소한 x 이상만큼 만들기 위해서 최소한으로 제거해야하는 돌멩이의 개수는 몇개인가?"
+( 또는 "점프력이 x 미만인 사람은 못건너게 할 때 최소한으로 제거해야하는 돌멩이의 개수는 몇개인가? " 라고 생각해도 좋습니다)
+"""
