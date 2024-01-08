c411f709a2aec8f5869ad8896bc073e67f00efa9
gunhong951 <gunhong951@gmail.com>
2024-01-08 16:58:02 +0900

    Re 대충만든자판

--- 대충만든자판.py
+++ 대충만든자판.py
@@ -10,7 +10,7 @@ def solution(keymap, targets):
         for s in target:
             if s in map:
                 count += map[s]
-            else: # 문자가 없다면 더이상 확인할 필요가 없다.
+            else:  # 문자가 없다면 더이상 확인할 필요가 없다.
                 result.append(-1)
                 flag = False
                 break
@@ -19,6 +19,27 @@ def solution(keymap, targets):
         result.append(count)
     return result
 
+
+def solution1(keymap, targets):
+    from collections import defaultdict
+
+    answer = []
+    keydic = defaultdict(int)
+    for key in keymap:
+        for i, c in enumerate(key, start=1):
+            keydic[c] = i if c not in keydic else min(i, keydic[c])
+    for target in targets:
+        tmp_count = 0
+        for ch in target:
+            if ch not in keydic:
+                tmp_count = -1
+                break
+            tmp_count += keydic[ch]
+        answer.append(tmp_count)
+    return answer
+
+
+print(solution1(["ABACD", "BCEFD"], ["ABCD", "AABB"]))
 
 # 어디서 오류가 발생한 것이지? 정확도가 절반나오네...
 # flag을 false로 했으면 true로 다시 되는 경우를 봐야지
