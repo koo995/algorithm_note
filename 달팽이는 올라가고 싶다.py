94c6aa5dc08877667f6e753932edb41ba70fd375
gunhong951 <gunhong951@gmail.com>
2024-01-08 16:38:46 +0900

    Re 달팽이는 올라가고 싶다

--- 달팽이는 올라가고 싶다.py
+++ 달팽이는 올라가고 싶다.py
@@ -20,3 +20,12 @@ print(count)
 
 # 좋은 것을 배워가는 문제같다. 단순히 반복문으로는 어렵지
 # 블로그에 있는 풀이는 허구한날 ㅅㅂ.. 똑같은 풀이밖에 없어
+def solutiuon():
+    import math
+
+    A, B, V = map(int, input().split())
+    c = math.ceil((V - A) / (A - B)) + 1
+    return c
+
+
+print(solutiuon())
