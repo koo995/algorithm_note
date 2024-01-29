fb5df19a806c37c9fbf989b75ff8b4d81a2d3c66
koo995 <107671886+koo995@users.noreply.github.com>
2024-01-29 10:59:41 +0900

    Re 베스트엘범

--- 베스트엘범.py
+++ 베스트엘범.py
@@ -1,21 +1,6 @@
 from functools import cmp_to_key
 
 
-# def lst_compare(a, b):
-#     # [[800, 3], [500, 0], [150, 2]] 이렇게 들어온다 했을때
-#     if a[0] > b[0]:
-#         return -1
-#     elif a[0] == b[0]:
-#         if a[1] < b[1]:
-#             return -1
-#         elif a[1] == b[1]:
-#             return 0
-#         else:
-#             return 1
-#     else:
-#         return 1
-
-
 def solution(genres, plays):
     dic = {}
     answer = []
@@ -29,12 +14,9 @@ def solution(genres, plays):
             continue
         dic[music_genre][0] += music_plays  # 총 플레이 시간을 계속 더해준다.
         dic[music_genre].append([music_plays, music_id])
-    print("dic: ", dic)
-    print("dict_items: ", dic.items())
     dic = sorted(
         dic.items(), key=lambda item: item[1][0], reverse=True
     )  # 총 시간 기준으로 정렬. 내림차순
-    print("sorted_dic: ", dic)
     for genre_info in dic:
         musics_sorted_by_plays = sorted(
             (genre_info[1][1:]), key=lambda x: (x[0], -x[1]), reverse=True
@@ -60,9 +42,61 @@ def solution(genres, plays):
 #         [500, 600, 150, 800, 2500, 500],
 #     )
 # )
+# print(
+#     solution(
+#         ["A", "A", "B", "A", "B", "B", "A", "A", "A", "A"],
+#         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#     )
+# )
+
+
+def solution2(genres, plays):
+    import heapq
+    from functools import cmp_to_key
+
+    def comparator(a, b):
+        if a[0] != b[0]:
+            return -1 if a[0] < b[0] else 1
+        else:  # a와 b가 같다면?
+            return 1 if a[1] < b[1] else -1
+
+    dic = {}
+    N = len(genres)
+    for i in range(N):
+        genre, play = genres[i], plays[i]
+        if genre not in dic:
+            dic[genre] = [
+                play,
+                [(play, i)],
+            ]  # [total, [(play, 0), (play, 1)]] 이런식으로 구성될 것이다.
+        else:
+            dic[genre][0] += play
+            heapq.heappush(dic[genre][1], (play, i))
+    dic_items = list(dic.items())
+    dic_items.sort(key=lambda lst: lst[1][0], reverse=True)
+    print("dic_items: ", dic_items)
+    result = []
+    for g, l in dic_items:
+        total, items = l
+        # items는 [(150, 2), (500, 0), (800, 3)] 이렇게 생겼다. heapq을 이용해서 기본적으로 작은녀석이 왼쪽에 있는데... 동일한 경우... 작은녀석도 왼쪽에 있다 하지만 원하는건 큰 녀석이 아니라 작은녀석
+        # 하... 여기서 새롭게 정렬을 할꺼면 heapq을 쓸 이유가 없는데... 문제를 처음부터 똑바로 읽을걸 그랫나?
+        items.sort(key=cmp_to_key(comparator))
+        count = 0
+        for item in items[::-1]:
+            if count == 2:
+                break
+            result.append(item[1])
+            count += 1
+    return result
+
+
+# dic:  {'classic': [[150, 2], [650, -1], [500, 0]], 'pop': [[600, 1], [600, -1]]} 이게 왜 이런 결과가 나온것일까?
 print(
-    solution(
-        ["A", "A", "B", "A", "B", "B", "A", "A", "A", "A"],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    solution2(
+        ["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]
     )
 )
+
+# heapq로 다루는 객체를 인덱스를 이용하여 임의로 조작하니까 뜻하지 않는 문제가 계속 발생했다
+# 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다. 이 조건을 판단하지 않았다.
+# 뭐 불필요하게 heapq을 쓰긴했지만... cmp_to_key을 사용해볼 좋은 기회였다
