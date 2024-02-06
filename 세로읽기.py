eaf527c00269c02dee733ebebdcac017a8415ad1
koo995 <107671886+koo995@users.noreply.github.com>
2024-02-06 14:35:01 +0900

    Re 세로읽기

--- 세로읽기.py
+++ 세로읽기.py
@@ -1,6 +1,17 @@
-words = [input() for _ in range(5)]
-for x in range(15):
-    for y in range(5):
-        if len(words[y]) <= x:
-            continue
-        print(words[y][x], end="")
+def solution():
+    words = [input() for _ in range(5)]
+    for x in range(15):
+        for y in range(5):
+            if len(words[y]) <= x:
+                continue
+            print(words[y][x], end="")
+
+
+def solution2(my_string: str, m, c):
+    answer = [
+        (lambda i: my_string[m * i + c - 1])(i) for i in range(len(my_string) // m)
+    ]
+    return "".join(answer)
+
+
+print(solution2("ihrhbakrfpndopljhygc", 4, 2))
