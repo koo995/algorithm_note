60efa12caceae2499173669ebe4ae310ebd03f60
koo995 <107671886+koo995@users.noreply.github.com>
2024-02-24 08:45:53 +0900

    re 올바른 괄호

--- 올바른 괄호.py
+++ 올바른 괄호.py
@@ -1,20 +1,38 @@
 from collections import deque
 
+
 def solution(s):
     answer = True
     stack = deque()
     for e in s:
         if e == "(":
             stack.append(e)
-        else: # )일때 stack에서 빼낸다. 근데 빼낼 것이 없으면 false?
+        else:  # )일때 stack에서 빼낸다. 근데 빼낼 것이 없으면 false?
             if stack:
                 stack.pop()
-            else :
+            else:
                 answer = False
-            
-    
+
     return answer
+
 
 print(solution("()()"))
 print(solution("(())()"))
 print(solution(")()("))
+
+
+def solution2(s):
+    stack = []
+    for a in s:
+        if a == "(":
+            stack.append(a)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+    if stack:
+        return False
+
+    else:
+        return True
