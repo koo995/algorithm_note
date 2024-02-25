51d6ba2448439a1d508649945d5c1f3f86cd84a5
koo995 <107671886+koo995@users.noreply.github.com>
2024-02-25 16:48:10 +0900

    re: 이중우선순위큐

--- 이중우선순위큐.py
+++ 이중우선순위큐.py
@@ -33,12 +33,41 @@ def solution(operations):
     return [0, 0]
 
 
+# 한가지 알아가는 것은
+# q라는 리스트안에서는 우선순위큐가 정렬이 되어 있지 않다는 것이아.
+
+
+def solution2(operations):
+    from collections import deque
+    import heapq
+
+    # 우선순위 큐는... 제일 작은 것이 앞에 오는 구나...
+    # 다만 그 뒷부분이 정렬이 된다는 것은 아니다.
+    h_min = []
+    h_max = []
+    for s in operations:
+        operation, digit = (lambda l: (l[0], int(l[1])))(s.split(" "))
+        if operation == "I":
+            heapq.heappush(h_min, digit)
+            heapq.heappush(h_max, -digit)
+            pass
+        if h_min and operation == "D":
+            # 최대값을 제거해야 한다.
+            if digit == 1:
+                pop_item = -heapq.heappop(h_max)
+                h_min.remove(pop_item)
+                # 그렇다면... 최소힙에서 어떻게 원소를 제거하지?
+            else:
+                # 최소값 삭제
+                pop_item = -heapq.heappop(h_min)
+                h_max.remove(pop_item)
+    if h_max or h_min:
+        return [-heapq.heappop(h_max), heapq.heappop(h_min)]
+    return [0, 0]
+
+
 print(
-    solution(
+    solution2(
         ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
     )
 )
-
-
-# 한가지 알아가는 것은
-# q라는 리스트안에서는 우선순위큐가 정렬이 되어 있지 않다는 것이아.
