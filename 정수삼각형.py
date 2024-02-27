3ce632a875f6976416dffdbefb606c2807b27092
koo995 <107671886+koo995@users.noreply.github.com>
2024-02-27 16:17:07 +0900

    re: 정수삼각형

--- 정수삼각형.py
+++ 정수삼각형.py
@@ -1,16 +1,20 @@
-n = int(input())
-costs = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * (i + 1) for i in range(n)]
-dp[0] = costs[0]
-for i in range(1, n):
-    dp[i][0] = dp[i - 1][0] + costs[i][0]
-for i in range(1, n):
-    l = len(costs[i])
-    for j in range(1, l):
-        dp[i][j] = (
-            max(dp[i - 1][j - 1], dp[i - 1][j]) if j != (l - 1) else dp[i - 1][j - 1]
-        ) + costs[i][j]
-print(max(dp[-1]))
+def solution0():
+    n = int(input())
+    costs = [list(map(int, input().split())) for _ in range(n)]
+    dp = [[0] * (i + 1) for i in range(n)]
+    dp[0] = costs[0]
+    for i in range(1, n):
+        dp[i][0] = dp[i - 1][0] + costs[i][0]
+    for i in range(1, n):
+        l = len(costs[i])
+        for j in range(1, l):
+            dp[i][j] = (
+                max(dp[i - 1][j - 1], dp[i - 1][j])
+                if j != (l - 1)
+                else dp[i - 1][j - 1]
+            ) + costs[i][j]
+    print(max(dp[-1]))
+
 
 # 제일 끝 부분에 있는 녀석은 max비교로 넣을 것이 아니군
 
@@ -31,4 +35,21 @@ def solution(triangle):
     return max(dp_triangle[-1])
 
 
-print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+# print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+
+
+def solution2():
+    n = int(input())
+    triangle = [list(map(int, input().split())) for _ in range(n)]
+    print("triangle: ", triangle)
+    for i, row in enumerate(triangle[1:], start=1):
+        for j in range(len(row)):
+            triangle[i][j] += max(
+                triangle[i - 1][j - 1] if j - 1 >= 0 else 0,
+                triangle[i - 1][j] if j < len(row) - 1 else 0,
+            )
+    print("trai: ", triangle)
+    print(max(triangle[-1]))
+
+
+solution2()
