3f7380791aa6048d9b9fcd874d5fd7a7d1e15352
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-01 14:57:55 +0900

    re: 크로아티아알파벳

--- 크로아티아 알파벳.py
+++ 크로아티아 알파벳.py
@@ -1,26 +1,41 @@
-cro_alphas = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-input_str = list(input()) # 100개 또는 그 이상? ljes=njak
-count = 0
-s = 0
-e = 0
-while( s <= e and s<len(input_str)):
-    if input_str[s] in ["c", "d", "l", "n", "s", "z"]:
-        if "".join(input_str[s:e+2]) in cro_alphas:
-            count += 1
-            s = e + 2
-            e = s
-            continue
-        elif "".join(input_str[s:e+3]) == "dz=":
-            count += 1
-            s = e + 3
-            e = s
-            continue
-        else:
-            s += 1
-            e = s
-            count += 1
-            continue
-    s += 1
-    e = s
-    count += 1
-print(count)
+def solution():
+    cro_alphas = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+    input_str = list(input())  # 100개 또는 그 이상? ljes=njak
+    count = 0
+    s = 0
+    e = 0
+    while s <= e and s < len(input_str):
+        if input_str[s] in ["c", "d", "l", "n", "s", "z"]:
+            if "".join(input_str[s : e + 2]) in cro_alphas:
+                count += 1
+                s = e + 2
+                e = s
+                continue
+            elif "".join(input_str[s : e + 3]) == "dz=":
+                count += 1
+                s = e + 3
+                e = s
+                continue
+            else:
+                s += 1
+                e = s
+                count += 1
+                continue
+        s += 1
+        e = s
+        count += 1
+    print(count)
+
+
+def solution2():
+    from string import ascii_lowercase
+
+    croa_dic = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+    s = input()  # 이 녀석은 최대 100 글자이다
+    for ch in croa_dic:
+        s = s.replace(ch, "*")
+    print("s: ", s)
+
+
+solution2()
