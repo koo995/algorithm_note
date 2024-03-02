79854a919e905eb29bf14cf8a9f1bfa8488d51f8
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-03 01:00:56 +0900

    re: 파괴되지않은 건물

--- 파괴되지않은건물.py
+++ 파괴되지않은건물.py
@@ -8,7 +8,14 @@ def solution(board, skills):
     print("total_affect: ", total_affect)
 
     # 모든 공격에 대한 계산?
-    for type, x1, y1, x2, y2, degree in skills:  # 모든 스킬들을 다 합한다음 보드에 적용한다!
+    for (
+        type,
+        x1,
+        y1,
+        x2,
+        y2,
+        degree,
+    ) in skills:  # 모든 스킬들을 다 합한다음 보드에 적용한다!
         # 이제부터 스킬하나하나에 대한 영향을 보드에 적용할 것이다.
         # 1일 경우는 적의 공격을 의미합니다. 건물의 내구도를 낮춥니다.
         total_affect[x1][y1] += degree if type != 1 else -degree
@@ -54,3 +61,31 @@ print(
 
 
 # 미쳤다는 말밖에 안나온다 이런 방법이 있다니 너무 신기하다
+
+
+def solution2(board, skills):
+    N = len(board)
+    M = len(board[0])
+    sum_affect = [[0] * (M + 1) for _ in range(N + 1)]
+
+    for type, r1, c1, r2, c2, degree in skills:
+        sum_affect[r1][c1] += degree if type == 2 else -degree
+        sum_affect[r1][c2 + 1] -= degree if type == 2 else -degree
+        sum_affect[r2 + 1][c1] -= degree if type == 2 else -degree
+        sum_affect[r2 + 1][c2 + 1] += degree if type == 2 else -degree
+
+    # 이제 누적합을 연산해줘야 한다. 두번에 걸쳐서 해줘야 한다. 먼저 왼쪽에서 오른쪽
+    for r in range(len(sum_affect)):
+        for c in range(1, len(sum_affect[0])):
+            sum_affect[r][c] += sum_affect[r][c - 1]
+    for r in range(1, len(sum_affect)):
+        for c in range(len(sum_affect[0])):
+            sum_affect[r][c] += sum_affect[r - 1][c]
+
+    # 살아남은 건물의 수를 카운트
+    count = 0
+    for r in range(len(board)):
+        for c in range(len(board[0])):
+            if board[r][c] + sum_affect[r][c] >= 1:
+                count += 1
+    return count
