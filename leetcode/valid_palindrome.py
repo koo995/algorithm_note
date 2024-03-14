8a37834b132bb72b737400c6ecb7c15b6e38b590
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-14 21:30:27 +0900

    add valid_palindrome

---
+++ leetcode/valid_palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 불필요한 것들은 제거한다.
+        stack = []
+        s_lower = ""
+        for ch in s:
+            if ch.isnumeric() or ch.isalpha():
+                s_lower += ch.lower()
+                stack.append(ch.lower())
+        s_palindrome = ""
+        while stack:
+            s_palindrome += stack.pop()
+        if s_lower == s_palindrome:
+            return True
+        else:
+            return False
+        # 어떤 자료구조를 쓸까 생각해봤는데 스택을 쓰면 후입선출이니 결과가 같다면 트루를 반환한다.
+
+
+sol = Solution()
+sol.isPalindrome("A man, a plan, a canal: Panama")
