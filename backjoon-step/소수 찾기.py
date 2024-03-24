4f3f2e5f9d568d3c159dda358e6b090c53d970b1
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-24 14:04:50 +0900

    add 소수 찾기

---
+++ backjoon-step/소수 찾기.py
@@ -0,0 +1,17 @@
+def solution():
+    def find_prime(n):
+        for i in range(2, n):
+            if num % i == 0:
+                return False
+        return True
+
+    N = int(input())
+    nums = list(map(int, input().split()))
+    count = 0
+    for num in nums:
+        if num != 1 and find_prime(num):
+            count += 1
+    print(count)
+
+
+solution()
