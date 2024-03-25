250259aa9297f5f7452f63779d40766a9f2237bb
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-26 02:27:23 +0900

    연결리스트 너무 좋다.

--- 표편집.py
+++ 표편집.py
@@ -8,21 +8,27 @@ def solution(n, k, cmds):
 
     def operate(q: deque, stack: list, cmd):
         if cmd[0] == "D":
-            q.rotate(-int(cmd[-1]))  # 음... 이것도 안에든 숫자가 크다면 그만큼 잡아먹긴 하는데
+            q.rotate(
+                -int(cmd[-1])
+            )  # 음... 이것도 안에든 숫자가 크다면 그만큼 잡아먹긴 하는데
             return
         elif cmd[0] == "U":
             q.rotate(int(cmd[-1]))
             return
-        elif cmd[0] == "C":  # 삭제하는 경우니까... 첫재녀석 삭제하는 경우, 가운데, 마지막 삭제하는 경우를 정해보자.
+        elif (
+                cmd[0] == "C"
+        ):  # 삭제하는 경우니까... 첫재녀석 삭제하는 경우, 가운데, 마지막 삭제하는 경우를 정해보자.
             if (
-                q[0] > q[1]
+                    q[0] > q[1]
             ):  # 여기서 가장 마지막인 녀석을 비교해야 하는데.. 딱 하나 남아서 그것마저 제거할 상황은 주어지지 않는다.
                 del_node = q.popleft()
                 prev_node = q[-1]  # 이 녀석이 사실상 나보다 위에 있는 녀석이다.
                 stack.append((del_node, prev_node))
                 q.rotate(1)
                 return
-            del_node = q.popleft()  # 여기서 다 삭제하는 경우는 남지가 않는데... 대신 하나만 남을 수 있다.
+            del_node = (
+                q.popleft()
+            )  # 여기서 다 삭제하는 경우는 남지가 않는데... 대신 하나만 남을 수 있다.
             prev_node = q[-1]
             stack.append((del_node, prev_node))  # 내가 삭제된 녀석
             return
@@ -32,7 +38,9 @@ def solution(n, k, cmds):
             prev_index = q.index(
                 prev_node
             )  # 여기서 시간을 꽤 잡아먹을까?... 이것을 위해서 q에서 현재 인덱스를 다 저장한다면 이동할때마다 수정해줘야하는 번거로움이 있다... 이건 아웃
-            q.insert((prev_index + 1) % len(q), del_node)  # 이것도 꽤 시간을 잡아먹을텐데?
+            q.insert(
+                (prev_index + 1) % len(q), del_node
+            )  # 이것도 꽤 시간을 잡아먹을텐데?
             return
 
     q = deque([i for i in range(n)])  # 제일 앞에 있는 녀석이 현 위치.
@@ -69,7 +77,9 @@ def solution1(n, k, cmds):
             pos, move = cmd.split(" ")
             q.rotate(-int(move))
         elif cmd[0] == "C":
-            if q[0] > q[1]:  # 이 조건이면 마지막 행인 것을 충족하는 가? 행은 항상 위에서 아래로 커질꺼야
+            if (
+                    q[0] > q[1]
+            ):  # 이 조건이면 마지막 행인 것을 충족하는 가? 행은 항상 위에서 아래로 커질꺼야
                 del_node = q.popleft()
                 prev_node = q[-1]
                 stack.append((del_node, prev_node))
@@ -117,7 +127,9 @@ def solution2(n, k, cmds):
         def __repr__(self):
             return self.__str__()
 
-    def operate(cmd, table, point, stack):  # point는 지역변수로써 주소값을 참고하지 않고 있다.
+    def operate(
+            cmd, table, point, stack
+    ):  # point는 지역변수로써 주소값을 참고하지 않고 있다.
         if cmd[0] == "D":
             # point가 아래로 내려가야 한다.
             for _ in range(int(cmd[-1])):
@@ -181,7 +193,9 @@ def solution3(n, point, cmds):
             self.prev = prev
             self.next = next
 
-    def operate(cmd, table, point, stack):  # point는 지역변수로써 주소값을 참고하지 않고 있다.
+    def operate(
+            cmd, table, point, stack
+    ):  # point는 지역변수로써 주소값을 참고하지 않고 있다.
         if cmd[0] == "D":
             # point가 아래로 내려가야 한다.
             for _ in range(int(cmd[-1])):
@@ -305,6 +319,69 @@ def solution4(n, point, cmds):
 # 일단 그 실패지점의 이유를 찾자. 모든 것이 다 삭제된 경우라면...? 그리고 복구해야 하는 상황이면?
 # class 로 만들어서 했는데... 또 테케에서 틀린다. 내가 분명 병신짓 하나 한거 같은데 도대체 뭐지
 # 하 병신인가... 완전히 문제를 잘못 이해했네... 위로 계속 올라가면 아래로 가는거 아니잖아
-print(
-    solution2(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"])
-)
+# print(
+#     solution2(8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"])
+# )
+
+
+def solution5(n, current_point, cmds):
+    class Node:
+        def __init__(self, prev_idx, next_idx, is_deleted=False):
+            self.prev = prev_idx
+            self.next = next_idx
+            self.is_deleted = is_deleted
+
+        def __repr__(self):
+            return f"Node(prev = {self.prev}, next = {self.next}, is_deleted = {self.is_deleted})"
+
+    def print_answer(t):
+        answer = ""
+        for node in t:
+            if node.is_deleted:
+                answer += "X"
+            else:
+                answer += "O"
+        print("answer: ", answer)
+
+    recent_deleted = []
+    table = [Node(i-1, i+1) for i in range(n)]
+    table[0].prev = None
+    table[-1].next = None
+    print("table: ", table)
+
+    for cmd in cmds:
+        print("current point: ", current_point)
+        if cmd[0] == "D":
+            _, n = cmd.split()
+            for _ in range(int(n)):
+                current_point = table[current_point].next
+        if cmd[0] == "U":
+            _, n = cmd.split()
+            for _ in range(int(n)):
+                current_point = table[current_point].prev
+        if cmd[0] == "C":
+            table[current_point].is_deleted = True
+            recent_deleted.append(current_point)
+            if table[current_point].prev is not None:
+                table[table[current_point].prev].next = table[current_point].next
+            if table[current_point].next is not None:
+                table[table[current_point].next].prev = table[current_point].prev
+            current_point = table[current_point].next if table[current_point].next is not None else table[current_point].prev
+        if cmd[0] == "Z":
+            idx = recent_deleted.pop()
+            table[idx].is_deleted = False
+            prev, next = table[idx].prev, table[idx].next
+            if prev is not None:
+                table[prev].next = idx
+            if next is not None:
+                table[next].prev = idx
+
+    answer = ""
+    for node in table:
+        if node.is_deleted:
+            answer += "X"
+        else:
+            answer += "O"
+    print("answer: ", answer)
+
+solution5(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"])
