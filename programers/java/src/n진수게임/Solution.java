1678b16e8b43459fa43a7a247bb65a2b57be205b
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-04 00:14:09 +0900

    자바에는 진수변환을 쉽게 해주는 것이 있다.

---
+++ programers/java/src/n진수게임/Solution.java
@@ -0,0 +1,20 @@
+package n진수게임;
+
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder nums = new StringBuilder();
+        for (int i = 0; nums.length() <= t * m; i++) {
+            nums.append(Integer.toString(i, n));
+        }
+        StringBuilder answer = new StringBuilder();
+        // 그렇다면 올라가는 이 숫자를 어떻게 정의할까?
+        for (int i = 1; answer.length() < t; i++){
+            if ((i - p) % m == 0) {
+                answer.append(nums.charAt(i - 1));
+            }
+        }
+        return answer.toString().toUpperCase();
+    }
+}
