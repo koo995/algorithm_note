c1669fdfb1314c5867be93edf5ba254e9622e1d0
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-05 15:03:58 +0900

    java 다트게임

--- programers/java/src/다트게임/Solution.java
+++ programers/java/src/다트게임/Solution.java
@@ -5,33 +5,30 @@ import java.util.*;
 class Solution {
     public int solution(String dartResult) {
         List<Integer> answer = new ArrayList<>();
-        List<Integer> bonusIdxs = new ArrayList<>();
-        // 정수가 잇는 위치의 모든 점의 인덱스를 찾는다. 아 잠시 점수가 10점 일수 있잖아
-        for (int i = 0; i < dartResult.length(); i++) {
-            char c = dartResult.charAt(i);
-            if (c == 'S' || c == 'D' || c == 'T') bonusIdxs.add(i);
-        }
-        for (int i = 0; i < bonusIdxs.size() ; i++) {
-            int bonusIdx = bonusIdxs.get(i);
-            int score = Integer.parseInt(dartResult.substring(i > 0 ? bonusIdxs.get(i - 1) +1 : 0 , bonusIdx));
-            if (dartResult.charAt(bonusIdx) == 'D') {
-                score = (int) Math.pow(score, 2);
-            } else if (dartResult.charAt(bonusIdx) == 'T') {
-                score = (int) Math.pow(score, 3);
-            }
-            if (bonusIdx + 1 < dartResult.length() && (dartResult.charAt(bonusIdx + 1) == '*' || dartResult.charAt(bonusIdx + 1) == '#')) {
-                if (dartResult.charAt(bonusIdx) == '*') {
-                    if (i > 0) {
-                        answer.set(i - 1, answer.get(i - 1) * 2);
-                    }
-                    score *= 2;
-                } else {
-                    score *= -1;
+        for (int i = 0 ; i < dartResult.length(); i++) {
+            if (Character.isDigit(dartResult.charAt(i))) {
+                int num = dartResult.charAt(i) - '0';
+                if (Character.isDigit(dartResult.charAt(i+1))) {
+                    num = 10;
+                    i++;
                 }
+                char option = i + 2 < dartResult.length() && !Character.isDigit(dartResult.charAt(i+2)) ? dartResult.charAt(i+2) : '0';
+                if (option == '*' && !answer.isEmpty()) {
+                    answer.set(answer.size() -1, answer.get(answer.size() -1) * 2);
+                }
+                answer.add(calc(num, dartResult.charAt(i+1), option));
             }
-            answer.add(score);
         }
-        return answer.stream().reduce(0, Integer::sum);
+        return answer.stream().reduce(0,Integer::sum);
+    }
+
+    static int calc(int num, char bonus, char option) {
+        Map<Character, Integer> bonusMap = Map.of('S', 1, 'D', 2, 'T', 3);
+        if (! (option == '0')) {
+            int result = (int) Math.pow(num, bonusMap.get(bonus));
+            return option == '*' ? result * 2 : result * (-1);
+        }
+        return (int) Math.pow(num, bonusMap.get(bonus));
     }
 }
 
