e7288fd0d4a9518024af2562263fc207b401b14a
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-16 02:06:54 +0900

    add 최소비용 구하기2

---
+++ backjoon-step/최소비용 구하기2.py
@@ -0,0 +1,43 @@
+def solution():
+    n = int(input()) # 도시의 개수 1000 이하
+    m = int(input()) # m개의 버스 10만 이하
+    bus_infos = [map(int, input().split()) for _ in range(m)]
+    start, end = map(int, input().split())
+    graph = [[] for _ in range(n + 1)] # 도시의 수가 n개 라면... 번호를 1번부터 매긴다.
+    for s, e, c in bus_infos:
+        graph[s].append((e, c))
+    
+    INF = int(1e9)
+    distance = [INF] * (n + 1)
+    pre_table = [i for i in range(n+1)] # pre_table의 초기값은 뭘로하지?
+    
+    def dijkstra(s):
+        import heapq
+        
+        distance[s] = 0
+        q = []
+        heapq.heappush(q, (distance[s], s))
+        while q:
+            cost, node = heapq.heappop(q)
+            if distance[node] < cost:
+                continue
+            for n_node, n_cost in graph[node]:
+                if distance[n_node] > n_cost + cost:
+                    distance[n_node] = n_cost + cost
+                    pre_table[n_node] = node
+                    heapq.heappush(q, (distance[n_node], n_node))
+
+    dijkstra(start)
+    path = [end]
+    now = end
+    while now != start:
+        now = pre_table[now]
+        path.append(now)
+    
+    
+    print(distance[end])
+    print(len(path))
+    print(" ".join(map(str, path[::-1])))
+
+
+solution()
