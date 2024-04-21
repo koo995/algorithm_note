1dcfa10532ed4db7d2472592dd49e538cbaa2f50
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-21 16:33:49 +0900

    add invert binary tree

---
+++ leetcode/tree/226. invert binary tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class Solution:
+    # bottom-up 방식
+    def invertTree2(self, root):
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+        return None
+    
+    # top-down 방식
+    def invertTre1(self, root):
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.left)
+                queue.append(node.right)
+        return root
+    
+    # dfs을 이용한 방식
+    def invertTree(self, root):
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.left)
+                stack.append(node.right)
+        return root
+
