91e9f5a615ed726fc14baaf16d253bdff95ef4e2
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-02 20:22:54 +0900

    add 요세푸스문제

---
+++ backjoon-step/요세푸스문제.py
@@ -0,0 +1,14 @@
+def solution():
+    from collections import deque
+    N, K = map(int, input().split())
+    
+    q = deque([i for i in range(1, N+1)])
+    answer = []
+    while q:
+        q.rotate(-(K-1))
+        answer.append(q.popleft())
+    
+    print("<" + ", ".join(map(str, answer)) + ">")
+
+
+solution()
