4e9983cce026140822f5c162810fbeaa5c0ea20d
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-03 21:37:02 +0900

    add 최소공배수

---
+++ backjoon-step/최소공배수.py
@@ -0,0 +1,15 @@
+def solution():
+    def get_min_mul(a, b):
+        max_yak = 1
+        for i in range(1, min(a, b) + 1):
+            if a % i == 0 and b % i == 0:
+                max_yak = max(max_yak, i)
+        return a * b // max_yak
+        
+    
+    T = int(input())
+    nums = [list(map(int, input().split())) for _ in range(T)]
+    for a, b in nums:
+        print(get_min_mul(a, b))
+    
+solution()
