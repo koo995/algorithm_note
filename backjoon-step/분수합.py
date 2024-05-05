ebf449048a5617ed9e280a5d83faff535c2a5f54
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-06 01:44:41 +0900

    add 분수합

---
+++ backjoon-step/분수합.py
@@ -0,0 +1,16 @@
+def solution():
+    import math
+    
+    a1, b1 = map(int, input().split())
+    a2, b2 = map(int, input().split())
+    b_max_yak = math.gcd(b1, b2)
+    a1, b1, a2, b2 = (a1 * b2) // b_max_yak, (b1 * b2) // b_max_yak, (a2 * b1) // b_max_yak, (b1 * b2) // b_max_yak
+    a = a1 + a2
+    b = b1
+    while math.gcd(a, b) != 1:
+        common_yak = math.gcd(a, b)
+        a = a // common_yak
+        b = b // common_yak
+    print(a, b)
+
+solution()
