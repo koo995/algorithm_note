07f550cbaa4bb797c3772ecb14a594df1c5b3ea7
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-09 22:16:09 +0900

    add 창문닫기

---
+++ backjoon-step/창문닫기.py
@@ -0,0 +1,12 @@
+def solution():
+    n = int(input()) # n은 26억까지 갈 수 있다
+    # 이것을... 최대 21억까지 연산하는것은 너무 크다.
+    # 다른 방법을 생각해보자.
+    i = 1
+    while 1:
+        if i**2 <= n < (i+1)**2:
+            print(i)
+            break
+        i += 1
+
+solution()
