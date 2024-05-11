e900f95f022ad83ecdfada26681d75d0d95cc6e0
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-11 21:13:35 +0900

    add 이항계수2

---
+++ backjoon-step/이항계수2.py
@@ -0,0 +1,13 @@
+def solution():
+    def factorial(n):
+        if n == 0:
+            return 1
+        dp[n] = n * factorial(n - 1)
+        return dp[n]
+        
+    N, K = map(int, input().split())
+    dp = [1] * (N + 1)
+    factorial(N)
+    print((dp[N] // dp[K] // dp[N-K]) % 10007 )
+
+solution()
