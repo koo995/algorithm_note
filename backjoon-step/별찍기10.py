823dac8e04f8009c7056c69fb5f191b74c534792
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-14 22:21:17 +0900

    별찍기10 정리

--- backjoon-step/별찍기10.py
+++ backjoon-step/별찍기10.py
@@ -1,36 +1,24 @@
 def solution():
-    import math
-    
-    N = int(input()) # N은 3의 거듭제곱 꼴이다. 3. 9. 27... 뭐... 몇번인지는 계속 3으로 나눠가면 되겠지?
-    # 이야 이거.. 예전 네이버에서 하노이탑과 같다.. print 문을 이용해서 어떻게 출력해나갈지 고민해보자.
-    # 한번에 프린트가 아닌... 프린트할 결과값을 어디다 저장할까?
-    # 빈칸은 다른 무언가로 대체하자
-    
     def star(n):
-        if n == 3 :
-            # 우선 여기에 2차원에 왜 슬라이싱이 들어가는지... 아아... 일단 제일 첫 녀석은 이모양이니까... 
-            print_map[0][:3] = print_map[2][:3] = [1]*3
-            print_map[1][:3] = [1, 0, 1]
+        if n == 3:
+            print_map[0][:3] = print_map[2][:3] = ["*", "*" ,"*"]
+            print_map[1][:3] = ["*"," ","*"]
             return
-        
-        a = n // 3
-        star(n//3) # 여기서 재귀를 먼저 실행한다는 것이 분할정복인가... 아래서 전부 합치나 본데?
+        # 만약에 아니라면은.. 더 깊숙히 들어가야지?
+        star(n//3)
+        inner_size = n//3
         for i in range(3):
             for j in range(3):
-                if i == 1 and j == 1:
+                if i == 1 and j == 1 :
                     continue
-                for k in range(a):# 복사해야할 패턴의 크기로 봐도 되겠군
-                    print_map[a * i + k][a * j:a * (j + 1)] = print_map[k][:a]
-    
-    print_map = [[0] * N for _ in range(N)] # N개 이면 N줄이 있겠지?
+                # 서서히 복사해 나가면서 조립해나가야한다.
+                for k in range(inner_size): # k는 행의 위치를 뜻한다. 아 그리고 중복덮어씌우기가 첫번째에 발생하지만... 그냥 넘어가자.
+                    print_map[inner_size * i + k][inner_size * j: inner_size * (j + 1)] = print_map[k][:3]
+        
+    N = int(input())
+    print_map = [[" "] * N for _ in range(N)]
     star(N)
-    for i in print_map :
-        for j in i :
-            if j :
-                print('*', end = '')
-            else :
-                print(' ', end = '')
-        print()
-    
+    for row in print_map:
+        print("".join(row))
 
 solution()
