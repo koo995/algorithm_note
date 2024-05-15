d5c7105189465d5f300b592b01a526fba671c076
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-15 23:00:33 +0900

    N 과 M (4)

---
+++ backjoon-step/N 과 M (4).py
@@ -0,0 +1,19 @@
+def solution():
+    def permutations(nums, M:int, path) -> list:
+        if M == 0: # M의 카운트를 줄여나가는 방법으로 가보자.
+            results.append(path)
+            return
+        
+        for idx, num in enumerate(nums):
+            tmp_path = path.copy()
+            tmp_path.append(num)
+            permutations(nums[idx:], M-1, tmp_path)
+            
+    N, M = map(int, input().split())
+    arrays = [i for i in range(1, N + 1)]
+    results = []
+    permutations(arrays, M, [])
+    for result in results:
+        print(*result)
+
+solution()
