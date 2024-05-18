d33d772708d2e3de2de4f01648ef2866bb8599cc
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-18 22:55:44 +0900

    add 신나는 함수 실행

---
+++ backjoon-step/신나는 함수 실행.py
@@ -0,0 +1,27 @@
+def solution():
+    def w(a, b, c):
+        if a <= 0 or b <= 0 or c <= 0:
+            dp[(a, b, c)] = 1
+            return 1
+
+        if a > 20 or b > 20 or c > 20:
+            dp[(a, b, c)] = dp[(20, 20, 20)] if (20, 20, 20) in dp else w(20, 20, 20)
+            return dp[(a, b, c)]
+
+        if a < b < c:
+            dp[(a, b, c)] = dp[(a, b, c - 1)] if (a, b, c - 1) in dp else w(a, b, c - 1) \
+                            + dp[(a, b - 1, c - 1)] if (a, b - 1, c - 1) in dp else w(a, b - 1, c - 1) \
+                            - (dp[(a, b - 1, c)] if (a, b - 1, c) in dp else w(a, b - 1, c))
+            return dp[(a, b, c)]
+        else:
+            r1 = dp[(a-1, b, c)] if (a-1, b, c) in dp else w(a - 1, b, c)
+            r2 = dp[(a-1, b-1, c)] if (a-1, b-1, c) in dp else w(a - 1, b - 1, c)
+            r3 = dp[(a-1, b, c-1)] if (a-1, b, c-1) in dp else w(a - 1, b, c - 1)
+            r4 = dp[(a-1, b-1, c-1)] if (a-1, b-1, c-1) in dp else w(a - 1, b - 1, c - 1)
+            dp[(a, b, c)] = r1 + r2 + r3 - r4
+            return dp[(a, b, c)]
+
+    dp = {}
+    print(w(1, 1, 1))
+
+solution()
