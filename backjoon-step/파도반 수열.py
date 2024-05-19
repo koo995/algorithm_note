19404487a42f7ca8df0ff72b4cbf4a25e3e55a0a
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-19 18:08:20 +0900

    파도반 수열

---
+++ backjoon-step/파도반 수열.py
@@ -0,0 +1,14 @@
+def solution():
+    T = int(input())
+    nums = [int(input()) for _ in range(T)]
+    dp = [0] * 101
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 1
+    dp[4] = 2
+    for i in range(5, 101):
+        dp[i] = dp[i - 1] + dp[i - 5]
+    for num in nums:
+        print(dp[num])
+
+solution()
