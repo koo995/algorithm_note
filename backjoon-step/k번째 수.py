e4b321e0d728d4c3acc16fa0e1f6b67e0f44bb4f
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-02 21:49:01 +0900

    add K 번째 수

---
+++ backjoon-step/k번째 수.py
@@ -0,0 +1,24 @@
+def solution():
+    def check(m: int) -> int:
+        # m이라는 숫자보다 작거나 같은 숫자들의 갯수를 세어보자
+        # i 번째 행에서 m 보다 작거나 같은 녀석들의 갯수를 구하고 더해보자
+        count = 0
+        for i in range(1, N + 1):
+            count += min(N, m//i)
+        return count
+
+    N = int(input())  # 10^5 이하
+    K = int(input())  # K는 최대 10^9 최소 1
+    # 이분탐색을 할 필요가 있을 듯 하다.배열의 값을 미리 정의하는 것은 힘들 것 같다.
+    s = 0  # check(s) != check(e)을 만족하기 위함
+    e = N * N  # 이렇게 잡아도 될까
+    while s + 1 < e:
+        mid = (s + e) // 2
+        if check(mid) < K:
+            s = mid
+        else:
+            e = mid
+    print(e)
+
+
+solution()
