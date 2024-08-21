15182e6e230841698996109febcbfba2a40b19f5
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-22 00:01:46 +0900

    re 가장 긴 증가하는 부분 수열2

--- backjoon-step/가장 긴 증가하는 부분 수열2.py
+++ backjoon-step/가장 긴 증가하는 부분 수열2.py
@@ -24,5 +24,35 @@ def solution():
             lis[idx] = num
     print(len(lis))
 
-solution()
+
+def solution2():
+    def find_greater_idx(n):
+        s = -1
+        e = len(lis)
+        while s + 1 < e:
+            m = (s + e) // 2
+            if lis[m] <= n:
+                s = m
+            else:
+                e = m
+        return e
+
+    N = int(input())
+    array = list(map(int, input().split()))
+    lis = [array[0]]
+    # 가장 긴 증가하는 수열을 찾아야한다.
+    if N == 1:
+        print(1)
+        return
+    for i in range(1, N):
+        if lis[-1] < array[i]:
+            lis.append(array[i])
+        else: # 만약에 같거나 더 작다면...? 어떤 값과 바꿔치기해서... 증가하는 수열로 만들면 되게 좋겠다. 그러면 누구랑 바꿀까?
+            idx = find_greater_idx(array[i])
+            lis[idx] = array[i]
+
+    print(len(lis))
+
+
+solution2()
 # 자... 구현은 얼핏하긴했는데... 논리적인 이유부터 찾아보자
