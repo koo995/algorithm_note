7a41d402252cc1a61abd2c7655c9b6ab1a2adcad
koo995 <107671886+koo995@users.noreply.github.com>
2024-08-21 20:48:18 +0900

    re 가장 긴 바이토닉 부분 수열

--- backjoon-step/가장 긴 바이토닉 부분 수열.py
+++ backjoon-step/가장 긴 바이토닉 부분 수열.py
@@ -21,4 +21,27 @@ def solution():
     print(d_dp[::-1])
     print(max((map(lambda t:t[0] + t[1], zip(i_dp, d_dp[::-1]))))-1)
 
-solution()
+def solution2():
+    N = int(input())
+    A = list(map(int, input().split()))
+    # 바이토닉에서 사장 긴 것을 할려면 특정 지점까지 가장 긴 증가하는 수열이 되어야 하고... 반대쪽에서 가장 긴 증가하는 수열이 되면 되겠네?
+    increase_dp = [1] * N
+    reverse_increase_dp = [1] * N
+
+    if N == 1:
+        print(1)
+        return
+    # 먼저 정방향 증가.
+    for i in range(1, N):
+        for j in range(i):
+            if A[i] > A[j]:
+                increase_dp[i] = max(increase_dp[i], increase_dp[j] + 1)
+    # 이제 역방향 증가
+    for i in reversed(range(N - 1)):
+        for j in reversed(range(i, N)):
+            if A[i] > A[j]:
+                reverse_increase_dp[i] = max(reverse_increase_dp[i], reverse_increase_dp[j] + 1)
+    print(max(list(map(lambda t: sum(t) - 1, zip(increase_dp, reverse_increase_dp)))))
+
+
+solution2()
