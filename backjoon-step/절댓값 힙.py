d9728e180def21dbf211e3465bef4bcaf5c0c1db
koo995 <107671886+koo995@users.noreply.github.com>
2024-09-19 16:10:49 +0900

    re 절댓값 힙

--- backjoon-step/절댓값 힙.py
+++ backjoon-step/절댓값 힙.py
@@ -1,15 +1,34 @@
-import sys
-import heapq
+# import sys
+# import heapq
+#
+# n = int(input())
+# q = []
+#
+# for i in range(n):
+#     a = int(sys.stdin.readline().rstrip())
+#     if a != 0:
+#         heapq.heappush(q, (abs(a), a))
+#     else:
+#         if not q:
+#             print(0)
+#         else:
+#             print(heapq.heappop(q)[1])
+#
+#
+def solution2():
+    import heapq
 
-n = int(input())
-q = []
-
-for i in range(n):
-    a = int(sys.stdin.readline().rstrip())
-    if a != 0:
-        heapq.heappush(q, (abs(a), a))
-    else:
-        if not q:
+    N = int(input())
+    nums = [int(input()) for _ in range(N)]
+    h = []
+    for num in nums:
+        if h and num == 0:
+            abs_value, value = heapq.heappop(h)
+            print(value)
+        elif num == 0:
             print(0)
-        else:
-            print(heapq.heappop(q)[1])
+        else: # 추가
+            heapq.heappush(h, (abs(num), num))
+
+solution2()
+
