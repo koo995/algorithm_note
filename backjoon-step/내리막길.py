d418a7c4ac2201efd92c95a94e4af1dce409aff6
koo995 <107671886+koo995@users.noreply.github.com>
2024-09-25 00:28:56 +0900

    re 내리막길

--- backjoon-step/내리막길.py
+++ backjoon-step/내리막길.py
@@ -30,8 +30,40 @@ def solution():
     dp[0][0] = dfs(0, 0)
     print(dp[0][0])
 
+def solution2():
+    import sys
+    sys.setrecursionlimit(10000)
 
+    def dfs(point):  # 하나 계속 걸리는게... 마지막지점은 무엇으로 체크하지?
+        if point == end:
+            return 1
+        y = point[0]
+        x = point[1]
+        # 이미 탐색한 적이 있다면 그 위치에서 목적지까지 갈수있는 길의 수를 반환한다.
+        if dp[y][x] != -1:
+            return dp[y][x]
+        # 이제 방문처리
+        dp[y][x] = 0
+        for i in range(4):
+            n_y = y + dy[i]
+            n_x = x + dx[i]
+            if not (0 <= n_y < M and 0 <= n_x < N and MAP[n_y][n_x] < MAP[y][x]):  # 여기서 방문을 체크하기보단... 탐색한다음 체크하자
+                continue
+            # 각 길에 대해서 탐색하고 갈 수 있는 길의 수를 더한다.
+            dp[y][x] += dfs((n_y, n_x))
+        return dp[y][x]
 
-solution()
+    M, N = map(int, sys.stdin.readline().split())  # 세로, 가로
+    MAP = [tuple(map(int, sys.stdin.readline().split())) for _ in range(M)]
+    dp = [[-1] * N for _ in range(M)]  # -1이면 아직 처리안함. 0이면 길이없는 것.
+    start = (0, 0)
+    end = (M - 1, N - 1)
+    dy = [1, -1, 0, 0]
+    dx = [0, 0, 1, -1]
+    # 확실히... 중복되는 연산이 존재하는데... 그 값들을 모두 업데이트한다..?
+    dfs(start)
+    print(dp[0][0])
+
+solution2()
 #  결과론적으로 답이 있을것이다 라고 생각한 것이 문제구나?
 # 답이 없을 수 있다. 그러면 어떻게 처리할 것이냐?특정지점에서 답이 없다는 것을 체크해야하는데... 방문하지 않은 경우는 -1로... 방문한 경우는 답이 없는 0으로 하자.
