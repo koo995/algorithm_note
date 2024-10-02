333e5de6b9c4c6d84dad43af97f605255672bd27
koo995 <107671886+koo995@users.noreply.github.com>
2024-10-03 01:09:26 +0900

    re 두 용액

--- backjoon-step/두 용액.py
+++ backjoon-step/두 용액.py
@@ -26,6 +26,36 @@ def solution():
 
     print(*answer)
 
+def solution2():
+    N = int(input())
+    array = list(map(int, input().split()))
+    array.sort()
 
+    # 이제부터 0에 가장 가까운 쌍을 찾아야 한다. -> 절댓값이 가장 작은 쌍을 찾아야한다.
+    # 그렇다면 절댓값?
+    # 양쪽에서 가면은...
+    # 0에 가장 가까운 수를 어떻게 만들어낼까? 어쨋든 두개를 더해야하고 그 값을 비교해야한다. 무엇이랑?
+    # 아... 결국엔 0에 가장 가까워져야 하는 것이고... 그 과정에서 최대한 가까운 녀석을 찾는 것이다.
+    s = 0
+    e = 1
+    min_value = int(3e10)
+    answer_pair = None
+    while 0 <= s < e < N:
+        l_1 = array[s]
+        l_2 = array[e]
+        sum_value = l_1 + l_2
+        if abs(sum_value) < min_value:
+            min_value = abs(sum_value)
+            answer_pair = (l_1, l_2)
+        if sum_value == 0:
+            print(l_1, l_2)
+            exit()
+        elif sum_value < 0:
+            s += 1
+            e += 1
+        else:
+            s -= 1
 
-solution()
+    print(*answer_pair)
+
+solution2()
