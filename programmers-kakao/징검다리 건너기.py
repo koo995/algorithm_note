b029b9e432201ebb565abb55e6ccec24666dcdd0
koo995 <107671886+koo995@users.noreply.github.com>
2024-10-04 12:37:34 +0900

    re 징검다리 건너기

--- programmers-kakao/징검다리 건너기.py
+++ programmers-kakao/징검다리 건너기.py
@@ -46,3 +46,42 @@ def solution2(stones, k):
         else:
             end = mid
     return start
+
+
+def solution3(stones, k):
+    def check(m, k):  # 최대 사이 거리라 하자.
+        zero = 0
+        for stone in stones:  # 심지어 i로 받고 stones[i] 로 하는 것도 통과못하고...
+            temp = stone - m + 1
+            if temp <= 0:
+                zero += 1
+            else:
+                if zero + 1 > k:
+                    return False
+                zero = 0
+        return True if zero + 1 <= k else False
+
+    # 이야... 여기서 참 최적화라니... 어마어마하네
+    def check1(m, k):
+        temp_stones = list(map(lambda stone: stone-m+1 if stone-m+1 >= 0 else 0, stones))
+        max_continuous_zero = 0
+        point = 0
+        continuous_zero = 0
+        while point < len(stones):
+            while point < len(stones) and stones[point] == 0:
+                point += 1
+                continuous_zero += 1
+                max_continuous_zero = max(max_continuous_zero, continuous_zero)
+            point += 1
+            continuous_zero = 0
+        return True if max_continuous_zero + 1 <= k else False
+
+    s = 0
+    e = max(stones) + 1
+    while s + 1 < e:
+        mid = (s + e) // 2
+        if check(mid, k):
+            s = mid
+        else:
+            e = mid
+    return s
