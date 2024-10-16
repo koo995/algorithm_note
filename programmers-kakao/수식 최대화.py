127cd9291458532fe341b9aaff07b6888651e6bc
koo995 <107671886+koo995@users.noreply.github.com>
2024-10-16 18:13:01 +0900

    re: 수식 최대화

--- programmers-kakao/수식 최대화.py
+++ programmers-kakao/수식 최대화.py
@@ -38,3 +38,64 @@ def solution(expression):
 
     answer = max_value
     return answer
+
+
+from itertools import permutations
+
+# 2번째 풀이..
+def calc_operator(operator, num1, num2):
+    if operator == "*":
+        return num1 * num2
+    elif operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+
+
+def calc(priority, operands, operators):
+    for target_operator in priority:  # 해봤자 최대 3개
+        while target_operator in operators:
+            idx = operators.index(target_operator)
+            result = calc_operator(target_operator, operands[idx], operands[idx + 1])
+            operands[idx] = result
+            operands.pop(idx + 1)
+            operators.pop(idx)
+    return abs(operands[0])
+
+
+def solution(expression):
+    operands = []
+    operators = []
+    temp = ""
+    for ch in expression:
+        if not ch.isnumeric():
+            operands.append(int(temp))
+            operators.append(ch)
+            temp = ""
+        else:
+            temp += ch
+    operands.append(int(temp))
+    max_value = 0
+    for priority in permutations(["*", "+", "-"], 3):
+        max_value = max(max_value, calc(priority, operands.copy(), operators.copy()))
+    return max_value
+
+
+# 3번째 풀이... 이런 방법도 있다...! eval이란거 신기하네
+from itertools import permutations
+
+def div_conqer(priority, expression, n):
+    if n == 2:
+        return str(eval(expression))
+    if priority[n] == "*":
+        return str(eval("*".join([div_conqer(priority, part, n + 1) for part in expression.split("*")])))
+    elif priority[n] == "+":
+        return str(eval("+".join([div_conqer(priority, part, n + 1) for part in expression.split("+")])))
+    elif priority[n] == "-":
+        return str(eval("-".join([div_conqer(priority, part, n + 1) for part in expression.split("-")])))
+
+def solution(expression):
+    max_value = 0
+    for priority in permutations(["*", "+", "-"], 3):
+        max_value = max(max_value, abs(int(div_conqer(priority, expression, 0))))
+    return max_value
