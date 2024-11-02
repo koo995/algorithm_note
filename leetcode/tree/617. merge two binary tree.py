5027f6b13c71a6cd8b0f9a151874af716c1a19c9
koo995 <107671886+koo995@users.noreply.github.com>
2024-11-03 00:43:54 +0900

    re: merge two binary tree

--- leetcode/tree/617. merge two binary tree.py
+++ leetcode/tree/617. merge two binary tree.py
@@ -13,3 +13,23 @@ class Solution:
             return node
         else:
             return root1 or root2
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(root1, root2):
+            if not root1 or not root2:
+                return root1 or root2
+            node = TreeNode(root1.val + root2.val)
+            node.left = dfs(root1.left, root2.left)
+            node.right = dfs(root1.right, root2.right)
+            return node
+
+        merged_tree = dfs(root1, root2)
+        return merged_tree
