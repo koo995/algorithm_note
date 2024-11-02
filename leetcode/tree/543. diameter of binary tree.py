a9ceabbb9d024da02a355bb8c679668fa5080f51
koo995 <107671886+koo995@users.noreply.github.com>
2024-11-02 21:59:47 +0900

    re: diameter of binary tree

--- leetcode/tree/543. diameter of binary tree.py
+++ leetcode/tree/543. diameter of binary tree.py
@@ -21,4 +21,26 @@ class Solution:
 
         dfs(root)
         return self.longest_distance
-        
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution2:
+    longest: int = 0
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # 가장 긴 직경의 길이를 구하면 된다. 즉 양쪽 depth의 최대 합을 구하면 될듯
+        def dfs(node) -> int:
+            if not node:
+                return -1
+            max_left = dfs(node.left)
+            max_right = dfs(node.right)
+            self.longest = max(self.longest, max_left + max_right + 2)
+            return max(max_left, max_right) + 1
+
+        dfs(root)
+        return self.longest
