7c2a1019cc2663008fb9faa13bc8433f4105bd85
koo995 <107671886+koo995@users.noreply.github.com>
2024-11-03 03:19:08 +0900

    re: balanced binary tree

--- leetcode/tree/110. balanced binary tree.py
+++ leetcode/tree/110. balanced binary tree.py
@@ -16,3 +16,27 @@ class Solution:
                 return -1
             return max(left, right) + 1
         return check(root) != -1
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+
+            if not root:
+                return 0
+            left_node = dfs(root.left)
+            right_node = dfs(root.right)
+            if left_node == -1 or right_node == -1:
+                return -1
+            if abs(left_node - right_node) > 1:
+                return -1
+            return max(left_node, right_node) + 1
+
+        result = dfs(root)
+        return True if result != -1 else False
