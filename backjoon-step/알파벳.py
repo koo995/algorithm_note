9027b6671a619c3b5b2b678ea5c3cf12e0f6b038
koo995 <107671886+koo995@users.noreply.github.com>
2024-12-23 21:47:37 +0900

    알파벳 dfs로 변경

--- backjoon-step/알파벳.py
+++ backjoon-step/알파벳.py
@@ -1,36 +1,40 @@
 from collections import deque
 
+max_dist = 0
 def solution():
-    M, N = map(int, input().split())
-    board = [input() for _ in range(M)]
-
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
-
-    visited = set()
-    visited.add(board[0][0])
-    q = deque()
-    q.append(((0, 0, 1, visited)))
-
-    max_dist = 1
-    while q:
-        y, x, d, cur_visited = q.popleft()
-        max_dist = max(max_dist, d)
+    global max_dist
+    def dfs(y, x, d):
+        global max_dist
+        # 종료조건...? 더이상 방문할 수 없다면 끝내야지
 
         for i in range(4):
             n_y = y + dy[i]
             n_x = x + dx[i]
 
-            # board을 나간다면 탐색 안함.
             if not (0 <= n_y < M and 0 <= n_x < N):
                 continue
-
-            # 지나간 알파벳도 안감.
-            if board[n_y][n_x] in cur_visited:
+            if visited[board[n_y][n_x]] == 1:
                 continue
-            next_visited = cur_visited.copy()
-            next_visited.add(board[n_y][n_x])
-            q.append((n_y, n_x, d + 1, next_visited))
+            visited[board[n_y][n_x]] = 1
+            dfs(n_y, n_x, d + 1)
+            visited[board[n_y][n_x]] = 0
+
+        max_dist = max(max_dist, d)
+
+    M, N = map(int, input().split())
+    board = [input() for _ in range(M)]
+
+    nodes = []
+    for row in board:
+        for ch in row:
+            nodes.append(ch)
+
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+
+    visited = {a: 0 for a in nodes}
+    visited[board[0][0]] = 1
+    dfs(0, 0, 1)
 
     print(max_dist)
 
