47a4081e1b423d995acfbd55852bf958ca74c9d5
koo995 <107671886+koo995@users.noreply.github.com>
2025-01-21 12:47:31 +0900

    re: 쉬운 계단 수

--- backjoon-step/쉬운계단수.py
+++ backjoon-step/쉬운계단수.py
@@ -35,4 +35,27 @@ def solution2():
         print(sum(dp[N][i] for i in range(10)) % int(1e9))
 
 
-solution2()
+def solution3():
+    N = int(input())
+
+    dp = [[0] * 10 for _ in range(N + 1)]
+
+    # 길이가 1이고 가장 오른쪽이 1~9인 i인 경우
+    for i in range(1, 10):
+        dp[1][i] = 1
+
+    # 길이가 size이고 가장 오른쪽이 0~9인 i인 경우
+    for size in range(2, N + 1):
+        for i in range(10):
+            if i == 0:
+                dp[size][i] = dp[size - 1][1]
+            elif i == 9:
+                dp[size][i] = dp[size - 1][8]
+            elif size == 2 and i == 1:
+                dp[size][i] = dp[size - 1][i + 1]
+            else:
+                dp[size][i] = dp[size - 1][i - 1] + dp[size - 1][i + 1]
+
+    print(sum(dp[N][i] for i in range(10)) % int(1e9))
+
+solution3()
