5f6e3fceb3a0d40708cf336682b6ee4bdc04702f
koo995 <107671886+koo995@users.noreply.github.com>
2025-01-27 22:17:00 +0900

    re: acm craft

--- backjoon-step/ACM craft.py
+++ backjoon-step/ACM craft.py
@@ -36,4 +36,37 @@ def solution():
         dp = [-1] * N  # 여기서 0000으로 초기화하면 왜 시간초과가 나타나는 것이지...?
         print(dfs(start))  # dfs가 무슨 역할을 할지 너가 명확히 정하지 못했어. 그러니까 어떤 값을 반환하는 함수일지.. 막연히 탐색해야겠다고만 생각했어.
 
-solution()
+def solution2():
+    def dfs(node):
+        if not graph[node]:
+            dp[node] = construction_time[node]
+            return construction_time[node]
+
+        if dp[node] != -1:
+            return dp[node]
+
+        # dp초기화를 어디서 해야할지 고민이네...
+        # 초기화를 하고 반환한다고 하면.. 딱 일관된거 같다.
+        max_time = 0
+        for n_node in graph[node]:
+            max_time = max(max_time, dfs(n_node))
+        dp[node] = max_time + construction_time[node]
+        return dp[node]
+
+    T = int(input())
+    for _ in range(T):
+        N, K = map(int, input().split())
+        construction_time = list(map(int, input().split()))
+        priorities = [list(map(lambda num: int(num) - 1, input().split())) for _ in range(K)]
+        dest = int(input()) - 1
+
+        graph = [[] for _ in range(N)]
+        for a, b in priorities:
+            graph[b].append(a) # b가 지어지기 위해서는 a가 지어져야함.
+
+        dp = [-1] * N
+        print(dfs(dest))
+
+
+
+solution2()
