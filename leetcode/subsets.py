311a891559f005200e91344a902e91f2e5326f93
koo995 <107671886+koo995@users.noreply.github.com>
2025-02-05 20:31:51 +0900

    re: subsets

--- leetcode/subsets.py
+++ leetcode/subsets.py
@@ -21,6 +21,31 @@ class Solution:
         results = []
         dfs(nums, [])
         return results
+
+    def subsets3(self, nums: List[int]) -> List[List[int]]:
+        answers = [[]]
+
+        # 여기서 모든 집합을 어떻게 구해야할까?
+        # 단순히 라이브러리를 사용하는 것이 아니라... 음... 있고 없고를 탐색해야겠는데?
+        def dfs(start, path):
+            answers.append(path)
+            if start == N - 1:
+                return
+
+            # 현재 탐색한 곳에서 기록을 이어간다.
+
+            for n_i in range(start, N):
+                # start번째를 이미 넣었으니까 그 다음부터 탐색한다.
+                if n_i == start:
+                    continue
+                dfs(n_i, path + [nums[n_i]])
+
+        N = len(nums)
+        for i in range(N):
+            # 여기서 탐색을 하면..
+            # i번째 녀셕이 포함된 집합들을 탐색하는 것이다.
+            dfs(i, [nums[i]])
+        return answers
     
 sol = Solution()
 sol.subsets([1,2,3])
