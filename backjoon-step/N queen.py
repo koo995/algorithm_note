71452375e8541a06afacc16bcf1a5ced077b68fe
koo995 <107671886+koo995@users.noreply.github.com>
2025-02-10 21:16:55 +0900

    re: n queen

--- backjoon-step/N queen.py
+++ backjoon-step/N queen.py
@@ -1,4 +1,6 @@
 # n = int(input())
+from scipy.special import factorial
+
 
 # ans = 0
 # row = [0] * n
@@ -59,60 +61,110 @@
 
 
 # 이 방식은 시간초과가 나타난다...
-def solution2():
-    def is_ok(y, x):
-        # 자 여기서 괜찮은지 어케 체크할까? 상하좌우대각선 체크해야한다.
-        for prev_y in range(y):
-            if Map[prev_y][x] == 1:
-                return False
-            for prev_x in range(len(Map)):
-                if Map[prev_y][prev_x] == 1 and abs(x - prev_x) == abs(y - prev_y):
-                    return False
-        return True
+# def solution2():
+#     def is_ok(y, x):
+#         # 자 여기서 괜찮은지 어케 체크할까? 상하좌우대각선 체크해야한다.
+#         for prev_y in range(y):
+#             if Map[prev_y][x] == 1:
+#                 return False
+#             for prev_x in range(len(Map)):
+#                 if Map[prev_y][prev_x] == 1 and abs(x - prev_x) == abs(y - prev_y):
+#                     return False
+#         return True
+#
+#     def dfs(y):
+#         if y == N:
+#             count.append(1)
+#             return
+#
+#         for x in range(N):
+#             Map[y][x] = 1
+#             if is_ok(y, x):
+#                 dfs(y + 1)
+#             Map[y][x] = 0
+#
+#     N = int(input())
+#     count = []
+#     Map = [[0] * N for _ in range(N)]
+#     dfs(0)
+#     print(sum(count))
+#
+# def solution3():
+#     def is_ok(y, x):
+#         # 자 여기서 괜찮은지 어케 체크할까? 상하좌우대각선 체크해야한다.
+#         for prev_y in range(y):
+#             if Row[prev_y] == x:
+#                 return False
+#             # 자 이제 대각선을 체크해야지?
+#             if Row[prev_y] != -1 and abs(Row[prev_y] - x) == abs(y - prev_y):
+#                 return False
+#         return True
+#
+#     def dfs(y):
+#         if y == N:
+#             count.append(1)
+#             return
+#
+#         for x in range(N):
+#             Row[y] = x
+#             if is_ok(y, x):
+#                 dfs(y + 1)
+#             Row[y] = -1
+#
+#     N = int(input())
+#     count = []
+#     Row = [-1] * N # 차원을 하나 줄여서...
+#     dfs(0)
+#     print(sum(count))
+#
+# def solution4():
+#     def is_ok(y, x):
+#         # 여기서 y, x가 가능한가?
+#         for prev_y in range(y):
+#             if dp[prev_y] == x or abs(y - prev_y) == abs(x - dp[prev_y]):
+#                 return False
+#         return True
+#
+#     def dfs(start_y):
+#         if start_y == N:  # start는 x값으로 점점 깊어진다.
+#             count.append(1)
+#             return
+#
+#         for x in range(N):
+#             if is_ok(start_y, x):
+#                 dp[start_y] = x
+#                 dfs(start_y + 1)
+#
+#     N = int(input())  # N이 15가 최대라면 이 경우의 최악의 시간복자도는 15팩토리얼이다... 그렇지만 가지치기를?
+#     dp = [-1] * N  # dp[x] = y
+#     count = []
+#     dfs(0)
+#     print(sum(count))
 
-    def dfs(y):
-        if y == N:
-            count.append(1)
-            return
+import sys
 
-        for x in range(N):
-            Map[y][x] = 1
-            if is_ok(y, x):
-                dfs(y + 1)
-            Map[y][x] = 0
+input = sys.stdin.readline
 
-    N = int(input())
-    count = []
-    Map = [[0] * N for _ in range(N)]
-    dfs(0)
-    print(sum(count))
+def is_ok(y, x):
+    for prev_y in range(y):
+        if dp[prev_y] == x or abs(y - prev_y) == abs(x - dp[prev_y]):
+            return False
+    return True
 
-def solution3():
-    def is_ok(y, x):
-        # 자 여기서 괜찮은지 어케 체크할까? 상하좌우대각선 체크해야한다.
-        for prev_y in range(y):
-            if Row[prev_y] == x:
-                return False
-            # 자 이제 대각선을 체크해야지?
-            if Row[prev_y] != -1 and abs(Row[prev_y] - x) == abs(y - prev_y):
-                return False
-        return True
+def dfs(start_y):
+    global count
+    if start_y == N:  # start는 x값으로 점점 깊어진다.
+        count += 1
+        return
 
-    def dfs(y):
-        if y == N:
-            count.append(1)
-            return
+    for x in range(N):
+        if is_ok(start_y, x):
+            dp[start_y] = x
+            dfs(start_y + 1)
 
-        for x in range(N):
-            Row[y] = x
-            if is_ok(y, x):
-                dfs(y + 1)
-            Row[y] = -1
+N = int(input())  # N이 15가 최대라면 이 경우의 최악의 시간복자도는 15팩토리얼이다... 그렇지만 가지치기를?
+dp = [-1] * N  # dp[x] = y
+count = 0
+dfs(0)
+print(count)
 
-    N = int(input())
-    count = []
-    Row = [-1] * N # 차원을 하나 줄여서...
-    dfs(0)
-    print(sum(count))
-
-solution3()
