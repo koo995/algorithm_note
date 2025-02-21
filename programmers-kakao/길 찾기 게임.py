1a1141b801cbad05656f97ca5ebeb053f37f6376
koo995 <107671886+koo995@users.noreply.github.com>
2025-02-21 23:18:38 +0900

    re: 길찾기 게임

--- programmers-kakao/길 찾기 게임.py
+++ programmers-kakao/길 찾기 게임.py
@@ -55,3 +55,57 @@ def solution(nodeinfo):  # x, y니까 유의하자.
 
     answer = [front_result, back_result]
     return answer
+
+
+import sys
+
+sys.setrecursionlimit(10000)
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"TreeNode(val={self.val}, left={self.left}, right={self.right})"
+
+
+# 이 문제의 생각의 방향은 루트노드를 구해야한다는 것이었고
+# 그리고 루트노드의 왼편은 모두 x값이 작은 것 오른편은 x값이 큰것이라는 기본 개념을 구현하면 되었다.
+def solution2(nodeinfo):  # x, y니까 유의하자.
+    def to_binary_serch_tree(nodeinfo):
+        if not nodeinfo:
+            return None
+
+        # nodeinfo.sort(key=lambda node: (-node[1]))
+        root = nodeinfo[0]
+        left = to_binary_serch_tree([node for node in nodeinfo if node[0] < root[0]])
+        right = to_binary_serch_tree([node for node in nodeinfo if node[0] > root[0]])
+        return TreeNode(root, left, right)
+
+    def preorder(treenode):
+        if not treenode:
+            return
+        result1.append(nodeinfo_dic[tuple(treenode.val)])
+        preorder(treenode.left)
+        preorder(treenode.right)
+
+    def postorder(treenode):
+        if not treenode:
+            return
+        postorder(treenode.left)
+        postorder(treenode.right)
+        result2.append(nodeinfo_dic[tuple(treenode.val)])
+
+    nodeinfo_dic = {tuple(node): idx + 1 for idx, node in enumerate(nodeinfo)}
+    nodeinfo.sort(key=lambda node: (-node[1]))
+    treenode = to_binary_serch_tree(nodeinfo)
+    result1, result2 = [], []
+    preorder(treenode)
+    postorder(treenode)
+    return [result1, result2]
+
+
+
