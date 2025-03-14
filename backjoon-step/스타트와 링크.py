4ac8296f51ff9d44fca677a0cf424ac7267e246e
koo995 <107671886+koo995@users.noreply.github.com>
2025-03-14 18:20:23 +0900

    re: 스타트와 링크

--- backjoon-step/스타트와 링크.py
+++ backjoon-step/스타트와 링크.py
@@ -28,5 +28,47 @@ def solution():
         min_balance = min(min_balance, abs(get_power(team_a) - get_power(team_b)))
     print(min_balance)
 
-solution()
+
+def solution2():
+    from itertools import combinations
+
+    def get_power(team) -> int:
+        # 팀은 최소 두명이다.
+        result = 0
+        for p1 in team:
+            for p2 in team:
+                if p1 == p2:
+                    continue
+                result += board[p1][p2]
+        return result
+
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    for i, j in zip(board, zip(*board)):
+        print(i, j)
+
+
+    sum_stat = [sum(i) + sum(j) for i, j in zip(board, zip(*board))]
+    nums = [i for i in range(N)]
+
+    min_diff = int(1e9)
+    for team_A in combinations(nums, N // 2):
+        team_B = set(nums) - set(team_A)
+        min_diff = min(min_diff, abs(get_power(team_A) - get_power(team_B)))
+
+    print(min_diff)
+
+# 이야... 이 풀이 대단하다.
+def solution3():
+    import sys
+    from itertools import combinations
+    N = int(sys.stdin.readline())
+    stat = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    sum_stat = [sum(i) + sum(j) for i, j in zip(stat, zip(*stat))]  # 대각선끼리 합
+    allstat = sum(sum_stat) // 2  # 모든 값 합의 절반
+    result = float('inf')
+    for l in combinations(sum_stat, N // 2):  # 대각선 합에서 뽑은 2개 중에서
+        result = min(result, abs(allstat - sum(l)))  # 모든 값의 절반 - 그 뽑은 2개 합의 차 = start팀 - link팀
+    print(result)
+solution2()
 
