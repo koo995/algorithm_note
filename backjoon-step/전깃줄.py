9853efb5f5666b4fe3bdf295667a103bdd70ae11
koo995 <107671886+koo995@users.noreply.github.com>
2025-03-14 19:55:05 +0900

    re: 전깃줄

--- backjoon-step/전깃줄.py
+++ backjoon-step/전깃줄.py
@@ -44,4 +44,43 @@ def solution2():
                 dp[i] = max(dp[i], dp[j] + 1)
     print(N - max(dp))
 
-solution2()
+def solution3():
+    N = int(input())  # 100개 이하
+    lines = [tuple(map(int, input().split())) for _ in range(N)]
+    lines.sort()
+    array = [b for a, b in lines]
+    # 이제부터 array에서 가장 긴 증가하는 부분 수열을 구해야한다.
+
+    # 우선 100밖에 안되지만... 엄청 큰 경우를 가정해보자.
+    # 초기 값을 무엇으로 하지?
+    dp = [1] * N
+    for i in range(N):
+        for j in range(i):
+            if array[i] > array[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    print(N - max(dp))
+
+# 만약 N이 100만이라 가정해보자. 그렇다면 훨씬 더 최적화를 해야한다.
+# 근데 이렇게 해도 시간은 똑같구만..
+def solution4():
+    from bisect import bisect_right
+
+    N = int(input())  # 100개 이하
+    lines = [tuple(map(int, input().split())) for _ in range(N)]
+    lines.sort()
+    array = [b for a, b in lines]
+    # 이제부터 array에서 가장 긴 증가하는 부분 수열을 구해야한다.
+
+    # 우선 100밖에 안되지만... 엄청 큰 경우를 가정해보자.
+    # 문제는 앞 녀석을 매번 탐색한다는 것이다.
+    stack = []
+    for i in range(N):
+        num = array[i]
+        insert_idx = bisect_right(stack, num)
+        if insert_idx == len(stack):
+            stack.append(num)
+        else:
+            stack[insert_idx] = num
+    print(N - len(stack))
+
+solution4()
