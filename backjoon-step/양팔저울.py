9dc8e2bdfdbe3a80fc3362ddaed3ed30cbb7f524
koo995 <107671886+koo995@users.noreply.github.com>
2025-03-19 21:34:33 +0900

    re: 양팔저울

--- backjoon-step/양팔저울.py
+++ backjoon-step/양팔저울.py
@@ -49,33 +49,36 @@ def solution3():
     weight_set = {0, chu_lst[0]}
     for i in range(1, len(chu_lst)):
         temp = set()
-        for weight in weight_set:
+        for weight in weight_set:  # 이것이 4만을 넘지 않을 것이란 생각때문일까?
             temp.add(weight + chu_lst[i])
             temp.add(abs(weight - chu_lst[i]))
         weight_set |= temp
     print(*["Y" if bead in weight_set else "N" for bead in bead_lst])
 
 def solution4():
-    N = int(input())  # 추의 갯수
+    chu_count = int(input())  # 30개 이하.
     chu_lst = list(map(int, input().split()))
-    max_chu = max(chu_lst)
-    M = int(input())  # 구슬의 갯수
-    bead_lst = list(map(int, input().split()))  # 구슬들
-    dp = [["N"] * N for _ in range(max_chu + 1)]
+    bead_count = int(input())
+    bead_lst = list(map(int, input().split()))
 
-    for w in range(max_chu):
-        for i in range(N):
-            cur_chu = chu_lst[i]
-            if cur_chu > w:
-                continue
-            if cur_chu == w:
-                dp[w][i] = "Y"
-            elif dp[w - cur_chu][i - 1] == "Y":
-                dp[w][i] = "Y"
-            elif
+    available_weight = {0, chu_lst[0]}
+    for i in range(1, chu_count):
+        chu = chu_lst[i]
+        temp = set()
+        for weight in available_weight:
+            temp.add(weight + chu)
+            temp.add(abs(weight - chu))
+        available_weight |= temp
+    answer = []
+    for bead in bead_lst:
+        if bead in available_weight:
+            answer.append("Y")
+        else:
+            answer.append("N")
+    print(*answer)
 
 
-solution3()
+solution4()
 # 1. 구슬의 모든 경우의수를 탐색해 나갈까? 낱개는 미리 y / n 로 정해놓는다 치면은 덧셈과 뺄셈만 모두 구하면 되는데...
 # 연산은 최대 2^500이다.... 적당한 가자치기를 한다해도... 줄이기는 쉽지 않아보이는데
 # 그냥 ㅆ 생각나는데로 하면 되는건데 계속 냅색이니 그런거나 다른 블로그에서 본 방식을 생각해서 꼬이는거 아녀
